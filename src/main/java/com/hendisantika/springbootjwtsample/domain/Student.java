231a545c2ff23e42f96dd84ee3bb2d8baf5d0f1f
Hendi Santika <hendisantika@gmail.com>
2018-08-01 07:38:52 +0700

    #3 - Add Student Domain

---
+++ src/main/java/com/hendisantika/springbootjwtsample/domain/Student.java
@@ -0,0 +1,68 @@
+package com.hendisantika.springbootjwtsample.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/08/18
+ * Time: 07.38
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName, lastName, email;
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName, String email) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
